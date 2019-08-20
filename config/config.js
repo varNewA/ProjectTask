259033fdc53af90f576b163ba47980ac41f6a22e
zhengyihui <varnewzyh@163.com>
2019-08-20 15:37:25 +0800

    newProject

---
+++ config/config.js
@@ -0,0 +1,8 @@
+/* export default {
+  routes: [
+    { path: '/', redirect: '/index' },  
+    { path:'/index',component: './Index' },
+    { path:'/login',component: './Login' },
+    { path:'/registe',component: './Registe' },
+  ]
+} */
